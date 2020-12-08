3bd8674d6c178558bdf2b25557eed65a285fb5d8
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-08 15:21:14 -0600

    commit tree f2b88970d9855a7615f3ea0a24e86c3075a87354

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index { |dwarf, i| puts "#{i + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.collect { |ring| ring.capitalize << '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(foods)
+  cheese_types = %w[cheddar gouda camembert]
+  foods.each { |food| return food if cheese_types.include?(food) }
+  nil
 end
